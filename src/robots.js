4ffb1f1d151a9aa56343cc2c0fbb85d7ad6830fb
Banele Ngwane <banelengwane@gmail.com>
2020-06-08 04:19:07 +0200

    Added files

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1, 
+        name: 'Bonae Mistir', 
+        username: 'Harlieem', 
+        email: 'hezagon@helna.rs'
+    },
+    {
+        id: 2, 
+        name: 'Penticos Jacobits', 
+        username: 'Pizzeria', 
+        email: 'heptagon@yaho.po'
+    },
+    {
+        id: 3, 
+        name: 'Qwesta Brine', 
+        username: 'Baritonia', 
+        email: 'mitch@bvza.sp'
+    },
+    {
+        id: 4, 
+        name: 'Banele Ngwane', 
+        username: 'Baneele', 
+        email: 'hezagon@helna.rs'
+    },
+    {
+        id: 5, 
+        name: 'Trezol Treble', 
+        username: 'Clerk', 
+        email: 'jomahno@sspee.or'
+    },
+    {
+        id: 6, 
+        name: 'Cheroke Amubae', 
+        username: 'Zisfort', 
+        email: 'yohaum@xiano.gs'
+    },
+    {
+        id: 7, 
+        name: 'Mikael Watson', 
+        username: 'Mikovits', 
+        email: 'helbron@gmail.uk'
+    },
+    {
+        id: 8, 
+        name: 'Finbihs Jweldear', 
+        username: 'Bancovi', 
+        email: 'zoxsian@murtuza.za'
+    },
+    {
+        id: 9, 
+        name: 'Appenia Copral', 
+        username: 'Arseele', 
+        email: 'chesterian@finlo.tu'
+    },
+    {
+        id: 10, 
+        name: 'Montrol Brahima', 
+        username: 'Sukwel', 
+        email: 'nisha@gon.av'
+    },
+    {
+        id: 11, 
+        name: 'Makeri Roani', 
+        username: 'Inepto', 
+        email: 'mahlopi@singato.bh'
+    },
+    {
+        id: 12, 
+        name: 'Zola Mahnatu', 
+        username: 'Roman', 
+        email: 'imnomia@ulkga.vn'
+    }
+];
