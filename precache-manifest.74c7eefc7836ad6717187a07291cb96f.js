3a16beaac5222edea5cfa63edfb5c97c9cfb02f7
Banele Ngwane <banelengwane@gmail.com>
2020-06-08 04:48:46 +0200

    Updates

---
+++ precache-manifest.74c7eefc7836ad6717187a07291cb96f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d6c6ec71463d4f9012489ff086b30f",
+    "url": "/banelengwane.github.io/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/banelengwane.github.io/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b4f1567d0ade58d01b97",
+    "url": "/banelengwane.github.io/robofriends/static/css/main.ebe14ac5.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/banelengwane.github.io/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/banelengwane.github.io/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f1567d0ade58d01b97",
+    "url": "/banelengwane.github.io/robofriends/static/js/main.d8f1cd17.chunk.js"
+  },
+  {
+    "revision": "be07a325e75b881ab0ea",
+    "url": "/banelengwane.github.io/robofriends/static/js/runtime-main.27a29520.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/banelengwane.github.io/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
